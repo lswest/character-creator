f333d0828df45a51d0c59ff7c757766eb6dc0a6f
Lucas Westermann <lucas.westermann@gmail.com>
2021-11-29 11:23:07 +0100

    exercise: finished all exercises (save stretch)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          background: "hsl(195deg, 20%, 86%)",
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          height: "40%",
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
